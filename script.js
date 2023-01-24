fa43c3788ff858ad51e2f1aceb55184aa1fbca1b
Ivan <joohny311@gmail.com>
2023-01-24 14:53:46 +0300

    Correction css, delete default city,

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
 
 const param = {
     "url": "https://api.openweathermap.org/data/2.5/",
-    "appid": "f1479a906a9a4d13cd63b398fbd33481"
+    "appid": "459fe78c900986dc14120406ac850b48",
 }
 
 let citySelect = document.querySelector('#city');
-const cities = {
-    703448: "Kyiv",
-    472045: "Voronezh",
-    524901: "Moscow",
-    625144: "Minsk",
-}
-
-function addingСities() {
-    let option;
-    for (let key in cities) {
-        option += `<option value="${key}">${cities[key]}</option>`;
-    }
-    citySelect.insertAdjacentHTML('afterbegin', option);
-}
-addingСities();
-
-let city = document.querySelector('.cityName');
+let btn = document.querySelector('button');
+let selectedCity = '';
+let cityName = document.querySelector('.cityName');
 let temp = document.querySelector('.temp');
-let wind = document.querySelector('.wind');
-let humidity = document.querySelector('.humidity');
-let pressure = document.querySelector('.pressure');
-let visibility = document.querySelector('.visibility');
+let wind = document.querySelector('.windStatCount');
+let humidity = document.querySelector('.humidityStatCount');
+let pressure = document.querySelector('.pressureStatCount');
+let visibility = document.querySelector('.visibilityStatCount');
+let weatherStatus = document.querySelector('.weatherStatus');
+let time = document.querySelector('.date');
 
+window.addEventListener('load', (e) => {
+    selectedCity = citySelect.value;
+    getWeather();
+})
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    selectedCity = citySelect.value;
+    getWeather();
+})
 
 
 function getWeather() {
-    const cityId = document.querySelector('#city').value;
-    fetch(`${param.url}weather?id=${cityId}&units=metric&APPID=${param.appid}`)
+    fetch(`${param.url}weather?q=${selectedCity}&appid=${param.appid}`)
         .then(weather => {
             return weather.json();
         }).then(showWeather);
 }
 
-function showWeather(data) {
-    console.log(data);
-    city.innerHTML = `${data.name}`;
-    temp.innerHTML = `Температура: ${Math.floor(data.main.temp)}&deg;`;
-    wind.innerHTML = `Скорость ветра: ${data.wind.speed} м/с`;
-    humidity.innerHTML = `Влажность: ${data.main.humidity}%`;
-    pressure.innerHTML = `Давление: ${data.main.pressure} мм рт. ст.`;
-    visibility.innerHTML = `Видимость: ${(data.visibility) / 1000} км`;
+function getWeatherPosition(position) {
+    fetch(`${param.url}weather?lat=${position.latitude}&lon=${position.longitude}&appid=${param.appid}`)
+        .then(weather => {
+            return weather.json();
+        }).then(showWeather);
 }
 
-getWeather();
-citySelect.onchange = getWeather;
+
+function showWeather(data) {
+    cityName.textContent = `${data.name}, ${data.sys.country}`;
+    temp.innerHTML = `Temp: ${Math.floor(data.main.temp - 273.15)}&deg;`;
+    wind.innerHTML = `<span>${data.wind.speed}</span> m/s`;
+    humidity.innerHTML = `<span>${data.main.humidity}</span>%`;
+    pressure.innerHTML = `<span>${Math.round(data.main.pressure / 1.33322)}</span> mmHg.`;
+    visibility.innerHTML = `<span> ${(data.visibility) / 1000}</span> km`;
+    weatherStatus.innerHTML= `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].main}">`;
+}
+
+navigator.geolocation.getCurrentPosition(success, error, {
+    enableHighAccuracy: true
+  })
+
+  function success({ coords }) {
+    getWeatherPosition(coords)
+
+  }
+  function error({ message }) {
+    console.log(message)
+  }
